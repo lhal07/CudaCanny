cef994918254701a7ffde587c21c0040d46c1627
Luis Lourenco <luis@inf.ufpr.br>
2010-05-27 16:39:39 -0300

    itkCudaCannyFilter.cxx: Adding some template information of the methods.

--- src/itkCudaCannyFilter.cxx
+++ src/itkCudaCannyFilter.cxx
@@ -9,39 +9,42 @@
 #include <stdlib.h>
 #include "itkImage.h"
 #include "itkPNGImageIO.h"
-#include "itkImageFileReader.h"
-#include "itkImageFileWriter.h"
 #include "itkCudaCannyFilter.h"
 
 namespace itk{
 
 /** Set the image used by the algorithm */
-void CudaCannyFilter::SetInput(ImgPixelType* input){
+template<class TInputImage> 
+void CudaCannyFilter<TInputImage>::SetInput(ImgPixelType* input){
   image = input;
 }
 
 /** Set the image dimensions used by the algorithm */
-void CudaCannyFilter::SetSize(int w, int h){
+template<class TInputImage> 
+void CudaCannyFilter<TInputImage>::SetSize(int w, int h){
   width = w;
   height = h;
 }
 
 /** Set the Variance parameter used by the Gaussian smoothing filter */
-void CudaCannyFilter::SetVariance(float v){
+template<class TInputImage> 
+void CudaCannyFilter<TInputImage>::SetVariance(float v){
   gaussianVariance = v;
   printf("%d \n",v);
 }
 
 /** Set the Maximum Gaussian Kernel Width parameter used by the Gaussian 
  * smoothing filter */
-void CudaCannyFilter::SetMaxKernelWidth(unsigned int kw){
+template<class TInputImage> 
+void CudaCannyFilter<TInputImage>::SetMaxKernelWidth(unsigned int kw){
   maxKernelWidth = kw;
 }
 
 /** Set the Two Thresholds parameters used by the Hysteresis */
-void CudaCannyFilter::SetThreshold(unsigned int T2, unsigned int T1){
+template<class TInputImage> 
+void CudaCannyFilter<TInputImage>::SetThreshold(unsigned int T2, unsigned int T1){
   Th = T2;
   Tl = T1;
 }
 
-}//end namespace
+}
